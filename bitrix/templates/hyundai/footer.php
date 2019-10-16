b5f8b6ca931a5339cc5014193c96cc808c73cf7c
neeil1990 <neeil@mail.ru>
2019-10-16 15:12:27 +0300

    no message

--- bitrix/templates/hyundai/footer.php
+++ bitrix/templates/hyundai/footer.php
@@ -95,8 +95,14 @@
                             </li>
                             <li class="footer__item">
                                 <a href="#" class="footer__link">Worldwide</a>
-                            </li> -->
-                           <!-- <li class="footer__item">
+                            </li>-->
+                            <li class="footer__item">
+                                <a href="/news" class="footer__link">Пресс-центр</a>
+                            </li>
+                            <li class="footer__item">
+                                <a href="/become-a-dealer" class="footer__link">Стать дилером</a>
+                            </li>
+                            <li class="footer__item">
                                 <a href="/history" class="footer__link">История бренда</a>
                             </li>
                             <li class="footer__item">
@@ -104,19 +110,19 @@
                             </li>
                             <li class="footer__item">
                                 <a href="/manufacturing" class="footer__link">Завод XMMP</a>
-                            </li>-->
+                            </li>
                             <li class="footer__item">
+                                <a href="/customer-services" class="footer__link">Сервисы для клиентов</a>
+                            </li>
+                            <!--<li class="footer__item">
                                 <a href="/news" class="footer__link">Пресс-центр</a>
                             </li>
-                           <!-- <li class="footer__item">
+                            <li class="footer__item">
                                 <a href="/vacancy" class="footer__link vacancy__link">Вакансии</a>
                             </li>
                             <li class="footer__item">
                                 <a href="/customer-services" class="footer__link">Сервисы для клиентов</a>
                             </li>-->
-                            <li class="footer__item">
-                                <a href="/become-a-dealer" class="footer__link">Стать дилером</a>
-                            </li>
                         </ul>
                     </li>
                     <li class="footer__column footer__column--last">
